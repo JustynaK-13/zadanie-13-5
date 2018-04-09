09ebce5c3eadc03b5738a8fdd8e82ecbed053598
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-04-09 13:43:35 +0200

    Fix index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var OSinfo = require('./modules/OSInfo');
+var OSinfo = require('./modules/OSInfo')
 OSInfo.print();
 
-function getOSinfo() {
-    var type = os.type();
-    if(type === 'Darwin') {
-        type = 'OSX';
-    } else if(type === 'Windows_NT') {
-        type = 'Windows';
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if(input !== null) {
+        var instruction = input.trim();
+        switch(instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+            case '/sayhello':
+                process.stdout.write('hello!\n');
+                break;
+            case '/getOSinfo':
+                getOSinfo();
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        };
     }
-    var release = os.release();
-    var cpu = os.cpus()[0].model;
-    var uptime = os.uptime();
-    var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
-}
-
-exports.print = getOSinfo;
+});
