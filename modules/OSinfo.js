dc7122461fd636cbd46d98cb39ee17e3480866eb
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-04-09 12:56:47 +0200

    Add colors to console

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -12,11 +12,11 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
+    console.log('System:'.grey, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
+    console.log('Uptime: ~'.green, (uptime / 60).toFixed(0), 'min');
+    console.log('User name:'.yellow, userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
 
